7147ccd9e3a147bf62049e9b80c9fc998d7b21c4
zkhiraoui <za.khiraoui@gmail.com>
2024-04-24 01:22:32 -0700

    zkhiraoui - alx  devops

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+"""
 from flask import Flask
-"""class Flask"""
-
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """displays text
-    Returns:
-        text
-    """
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """displays text
-    Returns:
-        text
-    """
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
